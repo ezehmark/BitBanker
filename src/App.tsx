751edcc31637940bbf58ca2a35e4ea4089957c05
ezehmark <ezehmark@gmail.com>
2025-05-05 06:00:42 +0100

    new build

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from "./home.tsx";
-import SignUp from "./signup.tsx";
+import Home from "./home.jsx";
+import SignUp from "./signup.jsx";
 import UploadPic from "./picupload.tsx";
-import Login from "./login.tsx";
+import Login from "./login.jsx";
 
 function App() {
   return (
