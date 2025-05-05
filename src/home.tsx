6cb8c4ac187422b9b812e933ec0e8c49e03e6c38
ezehmark <ezehmark@gmail.com>
2025-05-05 08:22:37 +0100

    new build

--- src/home.tsx
+++ src/home.tsx
@@ -19,6 +19,7 @@ function Home() {
 
   useEffect(()=>{
 const storedName = localStorage.getItem("fullName");
+const savedPicURL = localStorage.getItem("picURL");
 
 setName(storedName ?? "");
   setPicUrl(savedPicURL);
